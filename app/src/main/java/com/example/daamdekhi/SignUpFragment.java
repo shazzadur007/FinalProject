bf0eaf875074c8aaca28e44199837eba5abab4f7
xian-malik <bil.rogue@gmail.com>
2019-08-04 02:47:58 +0600

    ZUBAYER

--- app/src/main/java/com/example/daamdekhi/SignUpFragment.java
+++ app/src/main/java/com/example/daamdekhi/SignUpFragment.java
@@ -7,15 +7,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.EditText;
 
 
 public class SignUpFragment extends Fragment {
-
+    public static EditText Username;
+    public static EditText Password;
+    public static EditText Name;
+    public static EditText Nid;
+    public static EditText Address;
+    public static Double Latitude;
+    public static Double Longitude;
+    public static EditText Phone;
+    public static EditText Email;
+    private Context cont;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Username = container.findViewById(R.id.registerusername);
+        Password = container.findViewById(R.id.registerpassword);
+        Name = container.findViewById(R.id.registername);
+        Nid = container.findViewById(R.id.registernid);
+        Address = container.findViewById(R.id.registeraddress);
+        Phone = container.findViewById(R.id.registerphone);
+        Email = container.findViewById(R.id.registeremail);
+
+
+        Latitude = MainActivity.latitude;
+        Longitude = MainActivity.longitude;
+
+        createUser process = new createUser(cont);
+        process.execute();
+        
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_up, container, false);
     }
---
+++ app/src/main/java/com/example/daamdekhi/createUser.java
@@ -0,0 +1,76 @@
+package com.example.daamdekhi;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class createUser extends AsyncTask<Void, Void, Void> {
+    private String data = "";
+    Context cont;
+
+    public createUser(Context cont) {
+        super();
+        this.cont = cont;
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            String Username =  SignUpFragment.Username.getText().toString();
+            String Password =  SignUpFragment.Password.getText().toString();
+            String Name =  SignUpFragment.Name.getText().toString();
+            String Nid =  SignUpFragment.Nid.getText().toString();
+            String Address =  SignUpFragment.Address.getText().toString();
+            String Phone =  SignUpFragment.Phone.getText().toString();
+            String Email =  SignUpFragment.Email.getText().toString();
+
+            String u = "https://daamdekhi.com/api/user/create.php?userid="+Username+"&password="+Password+"&name="+Name+"&nid="+Nid+"&address="+Address+"&latitude=23&longitude=90&phoneno="+Phone+"&email="+Email+"";
+            URL url = new URL(u);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+
+            InputStream inputStream;
+            int status = httpURLConnection.getResponseCode();
+            if (status == HttpURLConnection.HTTP_OK) {
+                inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line = "";
+                while ((line = bufferedReader.readLine()) != null) {
+                    data = data + line;
+                }
+            } else {
+                Log.d("ERRORGET", "GET request not worked");
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+
+        Toast.makeText(cont, "User Created.", Toast.LENGTH_LONG).show();
+
+        SignUpFragment.Username.setText("");
+        SignUpFragment.Password.setText("");
+        SignUpFragment.Name.setText("");
+        SignUpFragment.Nid.setText("");
+        SignUpFragment.Address.setText("");
+        SignUpFragment.Phone.setText("");
+        SignUpFragment.Email.setText("");
+    }
+}
